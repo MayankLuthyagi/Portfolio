69245030c5e2980cf0f31b29edf16403058dba45
Mayank Singh <mayankrajputco75@gmail.com>
2025-01-13 23:26:38 +0530

    update

---
+++ Components/Center.jsx
@@ -0,0 +1,85 @@
+import '../src/index.css';
+export const Center = () => {
+    return (
+        <div className="flex-1 flex m-10 justify-center">
+            <div
+                className="w-auto h-auto border-2 border-white rounded-xl grey-color justify-start items-start text-white p-8"
+            >
+                <header>
+                    <h1 className="font-bold">Hey, I'm Mayank</h1>
+                    <div className="text-gray-400">
+                        <li>Completed MCA from JCBose UST, Faridabad</li>
+                        <li>Aspiring Full-Stack Java Developer with a passion for software development</li>
+                        <li>
+                            Currently developing{' '}
+                            <a href="https://github.com/MayankLuthyagi/FirstJob" className="font-bold text-white">
+                                OpportuneX
+                            </a>
+                            , a platform to support freshers in their career journey
+                        </li>
+                        <li>
+                            Backend Developer Intern at{' '}
+                            <a href="https://www.talentd.in/" className="font-bold text-white">
+                                Talentd
+                            </a>
+                            , contributing to backend solutions
+                        </li>
+                        <li>
+                            Open to new opportunities, feel free to connect via email - mayankluthyagico@gmail.com
+                        </li>
+                    </div>
+                </header>
+                <br/>
+                <section id="skills">
+                    <h2 className="font-bold">Skills</h2>
+                    <div className="skills-section text-gray-400">
+                        <li> Front-End Development : HTML, CSS, JavaScript, React and Figma</li>
+                        <li>Back-End Development : Java, Spring Boot, Docker, Postman, REST APIs</li>
+                        <li>Databases & Cloud Tools : AWS, MySQL, MongoDB and PostgreSQL</li>
+                        <li> Version Control & Collaboration: Git, GitHub, GitLab</li>
+                    </div>
+                </section>
+                <br/>
+                <section id="projects">
+                    <h2 className="font-bold">Side Projects</h2>
+                    <li className="text-gray-400">
+                        <a className="text-white font-bold" href="https://job-finder.streamlit.app/">
+                            Job Finder{' '}
+                        </a>
+                        Developed a web application that scrapes jobs from multiple platforms
+                    </li>
+                    <li className="text-gray-400">
+                        <a
+                            className="text-white font-bold"
+                            href="https://github.com/MayankLuthyagi/WhatsApp-Json-Message-Sender-Using-Selenium"
+                        >
+                            WhatsAppBot
+                        </a>{' '}
+                        Automated messaging to multiple WhatsApp communities using Selenium and JSON.
+                    </li>
+                </section>
+                <br/>
+                <section id="certifications">
+                    <h2 className="font-bold"> Certifications & Learning</h2>
+                    <div className="text-gray-400">
+                        <li>
+                            Java Spring Framework 6 with Spring Boot 3{' '}
+                            <a
+                                className="text-white font-bold"
+                                href="https://udemy-certificate.s3.amazonaws.com/image/UC-f8f63f34-c12c-48c8-a722-3ad9deffc5e0.jpg"
+                            >
+                                (Udemy)
+                            </a>
+                        </li>
+                        <li>
+                            Solved 500+ coding challenges across multiple platforms{' '}
+                            <a className="text-white font-bold" href="https://codolio.com/profile/MayankSingh">
+                                (Codolio)
+                            </a>
+                        </li>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+};
---
+++ Components/Sidebar.jsx
@@ -0,0 +1,50 @@
+import '../src/index.css';
+import '../src/App.css';
+
+export const Sidebar = () => {
+    return (
+        <div className="flex w-80 justify-center rounded-xl border-2 grey-color h-auto">
+            <div className="flex flex-col items-center m-8 h-auto">
+                <div>
+                    <img
+                        src="/passport.png"
+                        alt="Passport"
+                        className="rounded-full w-44 h-44"
+                    />
+                </div>
+                <div className="text-white mt-8 text-center">
+                    <h3 className="text-xl font-bold">Mayank Singh</h3>
+                    <hr className="my-6 border-gray-500 w-60" />
+                    <div className="flex justify-evenly my-4 w-full">
+                        <a href="https://www.linkedin.com/in/mayank012/" target="_blank">
+                            <img
+                                className="rounded-full border-2 w-8 h-8"
+                                src="/linkedin.png"
+                                alt="LinkedIn"
+                            />
+                        </a>
+                        <a href="mailto:mayankluthyagico@gmail.com" target="_blank">
+                            <img
+                                className="rounded-full border-2 w-8 h-8"
+                                src="/email.png"
+                                alt="Email"
+                            />
+                        </a>
+                        <a href="http://github.com/mayankluthyagi" target="_blank">
+                            <img
+                                className="rounded-full border-2 w-8 h-8"
+                                src="/github.png"
+                                alt="GitHub"
+                            />
+                        </a>
+                    </div>
+                    <hr className="my-2 border-gray-500 w-full" />
+                </div>
+                <h3 className="mt-4 text-xl font-bold text-white">Blogs</h3>
+                <div className="w-64 border-2 border-white rounded-xl mt-4 p-4 text-sm text-white text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. ...
+                </div>
+            </div>
+        </div>
+    );
+};
